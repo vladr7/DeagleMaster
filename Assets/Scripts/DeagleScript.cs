00956106540d9b2de546ee7b6d8cef33a32455d0
Vlad <ricean.vlad7@gmail.com>
2024-03-02 15:29:09 +0200

    Deagle SFX

--- Assets/Scripts/DeagleScript.cs
+++ Assets/Scripts/DeagleScript.cs
@@ -5,12 +5,13 @@ using UnityEngine.VFX;
 
 public class DeagleScript : MonoBehaviour
 {
-    public GameObject bulletPrefab; 
-    public Transform shootPoint; 
+    public GameObject bulletPrefab;
+    public Transform shootPoint;
     public int bulletSpeed;
     public List<ParticleSystem> shootSmokeList = new List<ParticleSystem>();
     public GameObject smokeUnderFeetPosition;
     public VisualEffect smokeUnderFeet;
+    public AudioClip shootSoundClip; // Change to AudioClip
 
     void Update()
     {
@@ -19,7 +20,7 @@ public class DeagleScript : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButtonDown("Fire1"))
         {
             ShootBullet(direction.normalized);
         }
@@ -33,11 +34,24 @@ public class DeagleScript : MonoBehaviour
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             if (rb)
             {
+                PlayGunshotSound();
                 PlaySmokeUnderFeetVFX();
                 PlayShootSmokeVFX();
                 rb.AddForce(direction * bulletSpeed);
             }
         }
+    }
+
+    private void PlayGunshotSound()
+    {
+        // Create a temporary GameObject to host the AudioSource component
+        GameObject tempAudioSource = new GameObject("TempAudio");
+        AudioSource audioSource = tempAudioSource.AddComponent<AudioSource>();
+        audioSource.clip = shootSoundClip;
+        audioSource.Play();
+
+        // Destroy the temporary GameObject after the clip finishes playing
+        Destroy(tempAudioSource, shootSoundClip.length);
     }
 
     private void PlaySmokeUnderFeetVFX()
@@ -49,14 +63,12 @@ public class DeagleScript : MonoBehaviour
 
     private void PlayShootSmokeVFX()
     {
-        int index = 0; 
+        int index = 0;
 
         while (index < shootSmokeList.Count)
         {
-            shootSmokeList[index].Play(); 
+            shootSmokeList[index].Play();
             index++;
         }
     }
-
-
 }
